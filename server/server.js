b48771a7c8ef6ef35f6480d13d2659ad9155efb4
iainu <iain@engagingnetworks.net>
2021-07-16 09:29:10 +0100

    initial commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require("socket.io")
+const io = new Server(server)
+
+app.use(express.static(__dirname + '/client'))
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/client/index.html')
+})
+
+let i = 1
+
+io.on('connection', socket => {
+
+	console.log('a user connected')
+
+	socket.on('spin', () => {
+
+		setTimeout(() => {
+			io.emit('spun', i)
+			i++
+		}, 1500)
+
+	})
+
+})
+
+server.listen(3000, () => {
+	console.log('listening on *:3000')
+})
